ce2991a303ecb914e64ae59b47bf943f6561dc63
cwxyz007 <jie844067636@gmail.com>
2018-03-26 13:16:46 +0800

    code refactoring

--- source/js/global.js
+++ source/js/global.js
@@ -1,7 +1,5 @@
 $(document).ready(()=> {
-  let pageName = location.pathname.match(/[a-z]+/)
-  if (!pageName) pageName = 'home'
-  else pageName = pageName.pop()
-  $('.nav-menu .' + pageName).addClass('active')
+  $('.simple-layout-nav').css({
 
+  })
 })
