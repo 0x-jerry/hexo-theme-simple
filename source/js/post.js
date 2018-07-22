923238d219f435f33e12659005a5996ad0400dc0
cwxyz <jie844067636@gmail.com>
2018-07-22 17:08:55 +0800

    add TOC active

--- source/js/post.js
+++ source/js/post.js
@@ -1,87 +1,122 @@
 $(document).ready(() => {
-  $('.markdown-content img').fancyImg()
+  $('.markdown-content img').fancyImg();
 
-  let $pageNav = $('#post-nav')
-  const $postContent = $('#post-content')
+  let $pageNav = $('#post-nav');
+  const $postContent = $('#post-content');
 
-  if($pageNav[0]) generateTOC($pageNav, $postContent)
-})
+  if ($pageNav[0]) generateTOC($pageNav, $postContent);
+
+  syncTOC($('.simple-layout-content'));
+});
+
+function syncTOC($content) {
+  const $elements = $('#post-content').find('h1, h2, h3, h4, h5, h6');
+  const max = $elements.length;
+  const calcActive = () => {
+    let mark = -1;
+
+    for (let i = 0; i < max; i++) {
+      const $ele = $($elements[i]);
+      if ($ele.offset().top >= 0) {
+        mark = i;
+        break;
+      }
+    }
+
+    if (mark != -1) {
+      const $lis = $('#post-nav').find('a');
+      const $ele = $(
+        $('#post-nav')
+          .find('a')
+          .get(mark)
+      );
+
+      $lis.removeClass('hover');
+      $ele.addClass('hover');
+    }
+  }
+
+  $($content).scroll(calcActive);
+  calcActive();
+}
 
 function getTOC($content) {
-  const getChildTOC = (list) => {
-    const level = list.node.level + 1
+  const getChildTOC = list => {
+    const level = list.node.level + 1;
 
     for (let index = 0; index < list.children.length; index++) {
       const item = list.children[index];
 
-      if (item.children.length !== 0) getChildTOC(item)
+      if (item.children.length !== 0) getChildTOC(item);
       else {
-          if (item.node.level !== level) {
-          const child = list.children.splice(index, 1)
+        if (item.node.level !== level) {
+          const child = list.children.splice(index, 1);
 
-          list.children[index - 1].children.push(child)
+          list.children[index - 1].children.push(child);
         }
       }
     }
-  }
+  };
 
-  const list = []
-  let preLevel = null
+  const list = [];
+  let preLevel = null;
 
   $content.find('h1, h2, h3, h4, h5, h6').each((index, ele) => {
-    const level = +ele.nodeName.split('').pop()
-    if (!preLevel) preLevel = level
-    const text = $(ele).text()
+    const level = +ele.nodeName.split('').pop();
+    if (!preLevel) preLevel = level;
+    const text = $(ele).text();
 
     const node = {
       level,
       text,
-      href: text.split(/[\s.*+?^=!:${}()|[\]/\\]+/).filter(item => item !== '').join('-'),
-    }
+      href: text
+        .split(/[\s.*+?^=!:${}()|[\]/\\]+/)
+        .filter(item => item !== '')
+        .join('-')
+    };
 
-    if(preLevel === level) {
+    if (preLevel === level) {
       list.push({
         node,
         children: []
-      })
+      });
     } else {
       list[list.length - 1].children.push({
         node,
         children: []
-      })
+      });
     }
-  })
-
-  list.forEach(child => {
-    getChildTOC(child)
   });
 
-  return list
+  list.forEach(child => {
+    getChildTOC(child);
+  });
+
+  return list;
 }
 
 function generateTOC($pageNav, $content) {
-
-  const generate = (list) => {
-    const $ul = $('<ul> </ul>')
+  const generate = list => {
+    const $ul = $('<ul> </ul>');
 
     list.forEach(child => {
-      const $li = $('<li> </li>')
+      const $li = $('<li> </li>');
 
-      $li.append($(`<a href='#${child.node.href}'>${child.node.text}</a>`))
+      $li.append($(`<a href='#${child.node.href}'>${child.node.text}</a>`));
 
       if (child.children.length !== 0) {
-        $li.append(generate(child.children))
+        $li.append(generate(child.children));
       }
 
-      $ul.append($li)
-    })
+      $ul.append($li);
+    });
 
-    return $ul
-  }
+    return $ul;
+  };
 
-  const TOC = getTOC($content)
+  const TOC = getTOC($content);
 
-  const $ul = generate(TOC)
+  const $ul = generate(TOC);
 
-  $pageNav.append($ul).addClass('active')
+  $pageNav.append($ul).addClass('active');
 }
