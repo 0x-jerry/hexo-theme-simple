e5f556eb785b5a54a2bdc9532ee1fd4d4eef760a
cwxyz <jie844067636@gmail.com>
2018-08-16 23:12:36 +0800

    code refactor

--- source/js/global.js
+++ source/js/global.js
@@ -1,14 +1,16 @@
-$(document).ready(()=> {
-  $(window).scroll(_.debounce( () => {
-    const top = $(window).scrollTop();
-    if (top < 50) {
-      $('nav').removeClass('active');
-    } else {
-      $('nav').addClass('active');
-    }
-  }))
+$(document).ready(() => {
+  $(window).scroll(
+    _.debounce(() => {
+      const top = $(window).scrollTop();
+      if (top < 50) {
+        $('nav').removeClass('active');
+      } else {
+        $('nav').addClass('active');
+      }
+    }),
+  );
 
-  $('.excerpt-link').click(function () {
+  $('.excerpt-link').click(function() {
     document.location.href = $(this).attr('href');
-  })
-})
+  });
+});
