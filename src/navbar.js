19b5a132a01f31a60f56902e7ae653ecc6523b73
Gem Xli <x.li.gem@gmail.com>
2020-03-11 11:57:52 +0800

    fix: Fix remove menu

--- src/navbar.js
+++ src/navbar.js
@@ -6,19 +6,16 @@ $(document).ready(() => {
       $('.menu')
         .find('.home, .home a')
         .addClass('hover')
-        .remove('hover')
       break
     case '/archives/':
       $('.menu')
         .find('.archives, .archives a')
         .addClass('hover')
-        .remove('hover')
       break
     case '/tags/':
       $('.menu')
         .find('.tags, .tags a')
         .addClass('hover')
-        .remove('hover')
       break
 
     default:
